aa4fff8531531e6c8bde50810055b19dd86a79fb
Alexander Bösch <sta.contribution@gmail.com>
2026-04-19 03:01:59 +0200

    Add nice bg to usp

--- app/components/Index/01/Index.vue
+++ app/components/Index/01/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="usp">
-    <UContainer class="flex flex-col min-h-[33vh] xl:min-h-[calc(100vh-var(--ui-header-height))]">
+    <UContainer class="bg-no-repeat bg-cover bg-[url(/img/lochlainn-riordan-G6CR8v_bts0-unsplash.jpg)] flex flex-col min-h-[33vh] xl:min-h-[calc(100vh-var(--ui-header-height))]">
           <div class="hidden md:flex py-8 flex-col gap-6 m-auto text-center max-w-[80ch]">
-            <h2 aria-label="Unique Selling Proposition" class="font-heading text-center text-4xl sm:text-5xl text-pretty tracking-tight font-bold text-highlighted leading-[1.2] uppercase">
+            <h2 aria-label="Unique Selling Proposition" class="font-heading text-white text-center text-4xl sm:text-5xl text-pretty tracking-tight font-bold text-highlighted leading-[1.2] uppercase">
               <typed class="hidden lg:inline-flex" :strings="[$t('usp.1'), $t('usp.2'), $t('usp.3')]" :typeSpeed="50">
                 <span class="typing"></span>
               </typed>
